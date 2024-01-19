8a8d7524cdc71061e9ba15ce5ab01023592b0206
Marcelo Rios <mrios@bevertec.com>
2024-01-18 20:13:24 -0500

    Adding memo error for rich text editor

--- src/components/rich-text-editor/RichTextEditor.js
+++ src/components/rich-text-editor/RichTextEditor.js
@@ -1,53 +1,37 @@
-import { useRef, memo, useCallback } from "react";
+import { useRef } from "react";
 import JoditEditor from "jodit-react";
 import "./RichTextEditor.css";
 
-const RichTextEditor = memo(
-  ({ value, setValue }) => {
-    const editor = useRef(null);
+const RichTextEditor = ({ value, setValue }) => {
+  const editor = useRef(null);
 
-    const config = {
-      readonly: false,
-      toolbar: true,
-      spellcheck: true,
-      language: "es",
-      toolbarButtonSize: "medium",
-      toolbarAdaptive: true,
-      showCharsCounter: true,
-      showWordsCounter: true,
-      enableDragAndDropFileToEditorenableDragAndDropFileToEditor: true,
-      showXPathInStatusbar: false,
-      askBeforePasteHTML: true,
-      askBeforePasteFromWord: true,
-      uploader: {
-        insertImageAsBase64URI: true,
-      },
-    };
+  const config = {
+    readonly: false,
+    toolbar: true,
+    spellcheck: true,
+    language: "es",
+    toolbarButtonSize: "medium",
+    toolbarAdaptive: true,
+    showCharsCounter: true,
+    showWordsCounter: true,
+    enableDragAndDropFileToEditorenableDragAndDropFileToEditor: true,
+    showXPathInStatusbar: false,
+    askBeforePasteHTML: true,
+    askBeforePasteFromWord: true,
+    uploader: {
+      insertImageAsBase64URI: true,
+    },
+  };
 
-    const handleChange = useCallback(
-      (newContent) => {
-        setValue(newContent);
-      },
-      [setValue]
-    );
-
-    return (
-      <JoditEditor
-        ref={editor}
-        value={value}
-        config={config}
-        tabIndex={1}
-        onChange={handleChange}
-      />
-    );
-  },
-  (prevProps, nextProps) => {
-    return (
-      prevProps.value === nextProps.value &&
-      prevProps.setValue === nextProps.setValue &&
-      JSON.stringify(prevProps.config) === JSON.stringify(nextProps.config)
-    );
-  }
-);
+  return (
+    <JoditEditor
+      ref={editor}
+      value={value}
+      config={config}
+      tabIndex={1}
+      onBlur={setValue}
+    />
+  );
+};
 
 export default RichTextEditor;
